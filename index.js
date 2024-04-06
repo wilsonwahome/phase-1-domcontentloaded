b6ae0ba14096725857d08ca399d24e3dcfc8ab9f
wahome <wahomewgitonga@gmail.com>
2024-04-06 19:10:05 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const paragraph = document.querySelector("p")
+    if (paragraph) {
+        paragraph.textContent = "This is really cool!"
+    }
+})
